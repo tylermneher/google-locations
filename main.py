a03ee578f726163a61dc3adf33401accc7a4e6d8
Tyler M. Neher <tyler@neherdata.com>
2023-05-10 14:18:20 -0400

    rm dumb shit

--- main.py
+++ main.py
@@ -16,12 +16,6 @@ print(locat[0])
 print(len(locat[0]))
 
 
-# for i in locat:
-# 	if len(i) == 6:
-# 		pass
-# 	else:
-# 		i["source"] = "NaN"
-
 df = pd.DataFrame.from_dict(locat)
 print(df.columns)
 print(df)
